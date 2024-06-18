5e9cc20beaa6ce2ae84b0724eba023e11d979dd6
Sean Brady <swangeon@gmail.com>
2024-06-18 07:30:22 -0700

    styles: Added type hinting and doc strings.

--- honeyscanner/active_attacks/fuzzing.py
+++ honeyscanner/active_attacks/fuzzing.py
@@ -1,6 +1,6 @@
 import time
 
-from .base_attack import BaseAttack
+from .base_attack import BaseAttack, AttackResults
 from boofuzz import (Session,
                      Target,
                      SocketConnection,
@@ -8,6 +8,7 @@ from boofuzz import (Session,
                      s_string,
                      s_delim,
                      s_get)
+from honeypots import BaseHoneypot
 
 """
 Notes:
@@ -19,15 +20,25 @@ A variable can be used to adjust the aggressiveness of the fuzzing - Future work
 
 
 class Fuzzing(BaseAttack):
-    def __init__(self, honeypot):
-        super().__init__(honeypot)
-        self.max_banner_length = 512
-        self.max_terminal_length = 1024
+    def __init__(self, honeypot: BaseHoneypot) -> None:
+        """
+        Initializes a new Fuzzing object.
 
-    def run_connection_fuzzing(self):
+        Args:
+            honeypot (BaseHoneypot): Honeypot object to get information
+                                     from for attack.
+        """
+        super().__init__(honeypot)
+        self.max_banner_length: int = 512
+        self.max_terminal_length: int = 1024
+
+    def run_connection_fuzzing(self) -> AttackResults:
         """
         Perform connection fuzzing by sending fuzzed ssh banners
         to the honeypot.
+
+        Returns:
+            AttackResults: Results of the attack.
         """
         try:
             s_initialize("fuzz_banner")
@@ -43,7 +54,7 @@ class Fuzzing(BaseAttack):
             session.connect(s_get("fuzz_banner"))
             session.fuzz()
 
-            test_cases_executed = session.total_mutant_index
+            test_cases_executed: int = session.total_mutant_index
 
             if self.is_honeypot_alive():
                 return (False,
@@ -58,10 +69,13 @@ class Fuzzing(BaseAttack):
                     f"Banner fuzzing failed: {e}",
                     0)
 
-    def run_terminal_fuzzing(self):
+    def run_terminal_fuzzing(self) -> AttackResults:
         """
         Perform terminal fuzzing by sending fuzzed terminal commands
         to the honeypot.
+
+        Returns:
+            AttackResults: Results of the attack.
         """
         try:
             s_initialize("fuzz_command")
@@ -77,7 +91,7 @@ class Fuzzing(BaseAttack):
             session.connect(s_get("fuzz_command"))
             session.fuzz()
 
-            test_cases_executed = session.total_mutant_index
+            test_cases_executed: int = session.total_mutant_index
 
             if self.is_honeypot_alive():
                 return (False,
@@ -92,12 +106,15 @@ class Fuzzing(BaseAttack):
                     f"Terminal fuzzing failed: {e}",
                     0)
 
-    def run_attack(self):
+    def run_attack(self) -> AttackResults:
         """
         Run both connection fuzzing and terminal fuzzing attacks.
+
+        Returns:
+            AttackResults: Results of the attack.
         """
         print(f"Running fuzzing attack on {self.honeypot.ip}:{self.honeypot.port}...")
-        start_time = time.time()
+        start_time: float = time.time()
 
         # terminal fuzzing
         success_terminal, message_terminal, test_cases_terminal = self.run_terminal_fuzzing()
@@ -107,12 +124,12 @@ class Fuzzing(BaseAttack):
         success_connection, message_connection, test_cases_connection = self.run_connection_fuzzing()
         print(success_connection, message_connection)
 
-        end_time = time.time()
-        time_taken = end_time - start_time
+        end_time: float = time.time()
+        time_taken: float = end_time - start_time
 
-        total_cases = test_cases_terminal + test_cases_connection
+        total_cases: int = test_cases_terminal + test_cases_connection
 
-        success = success_connection or success_terminal
-        message = f"{message_connection} - {message_terminal} - {total_cases} test cases executed"
+        success: str = success_connection or success_terminal
+        message: str = f"{message_connection} - {message_terminal} - {total_cases} test cases executed"
 
-        return success, message, time_taken, total_cases
+        return (success, message, time_taken, total_cases)
